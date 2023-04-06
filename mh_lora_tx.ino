75cec3d9f5918e79e2e2b72650a10aff0ed4c1b4
Erik Harg <erik@harg.no>
2023-04-06 23:35:55 +0200

    Version with temp sensing and CRC

--- mh_lora_tx.ino
+++ mh_lora_tx.ino
@@ -1,7 +1,28 @@
+/*
+  Temperature sensor to RESTDB.io collection
+  LoRa WAN version (part 1):
+  Temperature sensor via LoRa WAN MKR 1300 comms
+  By: Erik Harg <erik@harg.no>
+
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <SPI.h>
 #include <LoRa.h>
 #include <Adafruit_SleepyDog.h>
+#include <CRC32.h>
 #include "TimeLib.h"
+
+// Initialize Dallas Temperature sensors
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
 
 // Timekeeping
 unsigned long sendData = 0;         // next time we'll send data
@@ -9,7 +30,9 @@ unsigned long SEND_WAIT = 5;     // how long to wait between submissions -- 1800
 unsigned long LOOP_WAIT_MS = 5000;  // how long to wait between loops -- 2000 ms = 2 sec
 unsigned long lastLoopMillis = 0; // time of last loop execution
 
+// Storage between loops
 int counter = 0;
+String readBuffer = "";
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -30,7 +53,13 @@ void setup() {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+  Watchdog.reset();
+  
+  LoRa.enableCrc();
   Serial.println("LoRa started - starting service...");
+  
+  sensors.begin();
+  Serial.println("Service started");
   digitalWrite(LED_BUILTIN, LOW);
 }
 
@@ -60,16 +89,101 @@ void loop() {
 
 void sendDataNow() 
 {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+  String tempString = "";
+  String voltageString = "";
+  float tempVal = getTemp();
+  Watchdog.reset();
+  Serial.println("Got temp: " + String(tempVal));
 
-  // send packet
-  LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
+  if (tempVal != DEVICE_DISCONNECTED_C && tempVal > -127.0f)
+  {
+      tempString = tempAsString(tempVal);
+      Watchdog.reset();
 
-  counter++;
+      int sensorValue = analogRead(ADC_BATTERY);
+      // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 4.3V):
+      float voltage = sensorValue * (4.3 / 1023.0);
+      voltageString = formatVoltageAsString(voltage);
+      Serial.print("Sending temperature data:");
+      String sendString = tempString + ";" + voltageString;
+
+      unsigned int checksum = computeCRC(sendString);
+      Watchdog.reset();
+
+      sendString += ";" + String(checksum);
+      
+      Serial.println(sendString);
+
+      // send packet
+      LoRa.beginPacket();
+      LoRa.print(sendString.c_str());
+      LoRa.endPacket();
+
+      Serial.println("Sent, waiting for reply...");
+
+      Watchdog.reset();
+
+      delay(250);
+      
+      // try to parse response packet
+      readBuffer = "";
+      int packetSize = LoRa.parsePacket();
+      Watchdog.reset();
+      if (packetSize) {
+        // received a packet
+        Serial.print("Received packet '");
+    
+        // read packet
+        while (LoRa.available()) {
+          int msg = LoRa.read();
+          readBuffer += (char)msg;
+        }
+        Watchdog.reset();
+        Serial.print(readBuffer);
+    
+        // print RSSI of packet
+        Serial.print("' with RSSI ");
+        Serial.println(LoRa.packetRssi());
+
+        if (readBuffer.startsWith("ACK"))
+        {
+            Serial.println("Got response message:");  
+            Serial.println(readBuffer);
+            Serial.println("End of message");
+            sendData = now() + SEND_WAIT; // wait this long until we send data again
+        } else {
+            Serial.println("Got unsupported message:");
+            Serial.println(readBuffer);
+            Serial.println("End of message");
+        }
+        Watchdog.reset();
+      } else {
+        Serial.println("Could not receive response!");
+      }
+      Serial.println("Waiting until " + formatDateTime(sendData) + " to send data again");
+  }
+}
+
+float getTemp()
+{    
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    return sensors.getTempCByIndex(0);
+}
+
+String tempAsString(float tempC)
+{
+    String tempString = String(tempC);
+    char tempChars[6];
+    tempString.toCharArray(tempChars, 5);
+    return String(tempChars);
+}
+
+String formatVoltageAsString(float voltage)
+{
+    String tempString = String(voltage);
+    char vChars[4];
+    tempString.toCharArray(vChars, 4);
+    return String(vChars);
 }
 
 String formatDateTime(time_t t)
@@ -91,4 +205,18 @@ String formatDateTime(time_t t)
     String retval = y_s + "-" + mn_s + "-" + d_s + " " + h_s + ":" + mi_s + ":" + s_s + " UTC";
 
     return retval;
+}
+
+uint32_t computeCRC(String input)
+{
+  CRC32 crc;
+  char stringToCRC[255];
+  int len = input.length() + 1;
+  if(len > 255)
+    len = 254;
+  
+  input.toCharArray(stringToCRC, len);
+  
+  crc.add((uint8_t*)stringToCRC, len);
+  return crc.getCRC();
 }
